c775c12e0f631ab96888071a3e76453dea00a986
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-01-29 04:44:20 +0100

    modified configuration of load balancer

--- main.tf
+++ main.tf
@@ -245,7 +245,6 @@ resource "aws_elb" "web_elb" {
   } */
 
   health_check {
-    path                = "/health"
     target =  "HTTP:80/"
     interval            = 30
     timeout             = 5
